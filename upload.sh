33774c5b1da0b3680b525d337b10315ff7c036a4
Serano Colameo <serano@colameo.ch>
2016-11-24 07:51:53 +0100

    release changes

--- upload.sh
+++ upload.sh
@@ -18,9 +18,9 @@ echo "${BINTRAY_REPO}"
 echo "${PCK_NAME}"
 echo "${PCK_VERSION}"
 echo "${PATH_TO_REPOSITORY}"
-if [ ! -z "$PATH_TO_REPOSITORY" ]; then
+#if [ ! -z "$PATH_TO_REPOSITORY" ]; then
    cd $PATH_TO_REPOSITORY
-fi
+#fi
 FILES=./*
 BINARYDIR=./binary/*
 PLUGINDIR=./plugins/*
@@ -28,31 +28,32 @@ FEATUREDIR=./features/*
 for f in $FILES;
 do
 if [ ! -d $f ]; then
-  echo "Processing $f file..."
-  if [[ "$f" == *content.jar ]] || [[ "$f" == *artifacts.jar ]] 
+  if [[ "$f" == *content.jar ]] || [[ "$f" == *artifacts.jar ]] || [[ "$f" == p2.index ]] 
   then
-    echo "Uploading p2 metadata file directly to the repository"
-    curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/$f;publish=0
-  else 
-    curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0
+  export ff=$(echo $f | cut -c3-1000)
+  echo "Processing $ff file..."
+#    echo "Uploading p2 metadata file directly to the repository"
+#    curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/$f;publish=0
+#  else 
+    curl -X PUT -T $ff -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$ff;publish=0
   fi
-  echo ""
+#  echo ""
 fi
 done
-echo "Processing features dir $FEATUREDIR file..."
 for f in $FEATUREDIR;
 do
-  echo "Processing feature: $f file..."
-  curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0
-  echo ""
+  export ff=$(echo $f | cut -c3-1000)
+  echo "Processing feature: $ff file..."
+  curl -X PUT -T $ff -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$ff;publish=0
+#  echo ""
 done
-echo "Processing plugin dir $PLUGINDIR file..."
 for f in $PLUGINDIR;
 do
    # take action on each file. $f store current file name
-  echo "Processing plugin: $f file..."
-  curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0
-  echo ""
+  export ff=$(echo $f | cut -c3-1000)
+  echo "Processing plugin: $ff file..."
+  curl -X PUT -T $ff -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$ff;publish=0
+#  echo ""
 done
 echo "Publishing the new version"
 curl -X POST -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/publish -d "{ \"discard\": \"false\" }"
