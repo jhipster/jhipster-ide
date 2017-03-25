c3dec1a3a9f697179ffbcfb70b5ce411472573e0
Serano Colameo <serano@colameo.ch>
2017-03-25 12:09:07 +0100

    retry 1

--- travis/scripts/deploy.sh
+++ travis/scripts/deploy.sh
@@ -2,14 +2,16 @@
 set -e
 
 BUILD_DIR=$1
-BUILD_TYPE=$2
-BUILD_TAG=$3
+BUILD_TAG=$2
 
 echo $BUILD_DIR
-echo $BUILD_TYPE
 echo $BUILD_TAG
+
 cd $BUILD_DIR
 
 java -version
 ./mvnw -version
 node --version
+
+cd $BUILD_DIR/jdl/io.github.jhipster.jdl.vscode 
+npm install && vsce package
