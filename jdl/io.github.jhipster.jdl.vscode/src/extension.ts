403d251b79f9006c650f4c24e99da112c1748ba6
Serano Colameo <serano@colameo.ch>
2018-08-29 22:00:51 +0200

    added jh to doc selector

--- jdl/io.github.jhipster.jdl.vscode/src/extension.ts
+++ jdl/io.github.jhipster.jdl.vscode/src/extension.ts
@@ -42,7 +42,7 @@ export function activate(context: ExtensionContext) {
     // Options to control the language client
     let clientOptions: LanguageClientOptions = {
         // Register the server for plain text documents
-        documentSelector: ['jdl'],
+        documentSelector: ['jdl', 'jh'],
         synchronize: {
             // Synchronize the setting section 'languageServerExample' to the server
             configurationSection: 'languageServerExample',
